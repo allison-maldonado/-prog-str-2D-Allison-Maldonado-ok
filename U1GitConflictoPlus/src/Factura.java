bea0335ca4dc86dd0b810b28df8a4c926e8a24a5
Allison Maldonado <20253ds096@utez.edu.mx>
2026-01-23 09:14:32 -0600

    merge:cambio para conflicto

--- U1GitConflictoPlus/src/Factura.java
+++ U1GitConflictoPlus/src/Factura.java
@@ -1,7 +1,6 @@
 public class Factura {
 
-    public static final String VERSION_OBJETIVO = "PENDIENTE";
-
+    public static final String VERSION_OBJETIVO = "merge";
     private static final double IVA = 0.16;
     private static final double PRECIO_UNITARIO = 1200.0;
 
