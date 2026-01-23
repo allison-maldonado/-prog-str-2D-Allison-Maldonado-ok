b4b8f25c1ae16440311b558abd6f8a25b4467f68
Allison Maldonado <20253ds096@utez.edu.mx>
2026-01-23 08:48:18 -0600

    init: proyecto U1GitConflicoPlus

---
+++ U1GitConflictoPlus/src/Factura.java
@@ -0,0 +1,12 @@
+public class Factura {
+
+    public static final String VERSION_OBJETIVO = "PENDIENTE";
+
+    private static final double IVA = 0.16;
+    private static final double PRECIO_UNITARIO = 1200.0;
+
+    public static double calcularTotal(int cantidad) {
+        double subtotal = cantidad * PRECIO_UNITARIO;
+        return subtotal + (subtotal * IVA);
+    }
+}
