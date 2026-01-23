bb9b93d0749ca18259a1fc70008687824ddacb1a
Allison Maldonado <20253ds096@utez.edu.mx>
2026-01-23 09:12:40 -0600

    develope:cambio para conflicto

--- U1GitConflictoPlus/Factura.java
+++ U1GitConflictoPlus/Factura.java
@@ -1,7 +1,6 @@
 public class Factura {
 
-    public static final String VERSION_OBJETIVO = "develop";
-    private static final double IVA = 0.16;
+    public static final String VERSION_OBJETIVO = "develop";    private static final double IVA = 0.16;
     private static final double PRECIO_UNITARIO = 1200.0;
 
     public static double calcularTotal(int cantidad) {
